a737bcb1fe41aad7db7f5daee558800b7a4a6b66
VALDEZ R. <renzv753@gmail.com>
2024-10-16 08:37:54 +0800

    Create Object activity.js

---
+++ Object activity.js
@@ -0,0 +1,15 @@
+const car = {
+  type: "Honda",
+  model: "Civic",
+  color: "Blue",
+};
+console.log(typeof car); 
+
+car.type = ("Toyota");
+console.log(car.type);  { 
+  
+}
+car.wheels = 4;
+console.log(car.wheels);  { 
+  
+}
